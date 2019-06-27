5bc8eb1eda501d3dfd6aba2d9fca958db8a3e354
Crispin Merriman <crispin.merriman@gmail.com>
2019-06-27 22:55:21 +0100

    Remove dummy number used for testing :facepalm:

--- src/components/summary/Summary.tsx
+++ src/components/summary/Summary.tsx
@@ -50,7 +50,7 @@ class Summary extends React.Component<Props, {}> {
   render() {
     const timeOfLatestFeed = this.getTimeOfLatestFeed()
     const showFeedWarning =
-      differenceInHours(timeOfLatestFeed, new Date().getTime() + 10000000000) <
+      differenceInHours(timeOfLatestFeed, new Date().getTime()) <
       -maxHoursWithoutFeed
 
     if (timeOfLatestFeed === 0) {
